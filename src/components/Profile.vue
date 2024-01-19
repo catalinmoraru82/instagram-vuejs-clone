ae083bfcfbd57adf1f13c910ea0bdd0b2c53cc8b
Catalin Moraru <catalin.moraru@everymatrix.com>
2024-01-19 18:26:52 +0200

    add userBar component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,20 +1,27 @@
 <template>
     <Container>
         <div class="profile-container">
-            <h1>userbar</h1>
+            <UserBar
+                username="SomeName"
+                :userinfo="{
+                    posts: 4,
+                    followers: 100,
+                    following: 343
+                }"
+            />
             <h1>image gallery</h1>
         </div>
     </Container>
 </template>
 <script setup>
-
+    import Container from './Container.vue';
+    import UserBar from './UserBar.vue';
 </script>
 
 <style scoped>
 .profile-container {
     display: flex;
     flex-direction: column;
-    align-items: center;
     padding: 20px 0;
 }
 </style>
---
+++ src/components/UserBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="userbar-container">
+        <div class="top-content">
+            <a-typography-title :level="2">{{ props.username }}</a-typography-title>
+        </div>
+        <div class="bottom-content">
+            <a-typography-title :level="5">{{ props.userinfo.posts }} Posts</a-typography-title>
+            <a-typography-title :level="5">{{ props.userinfo.followers }} Followers</a-typography-title>
+            <a-typography-title :level="5">{{ props.userinfo.following }} Following</a-typography-title>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps(['username', 'userinfo'])
+</script>
+<style scoped>
+.userbar-container {
+    padding-bottom: 75px;
+}
+
+.bottom-content {
+    display: flex;
+    align-items: center;
+}
+
+.bottom-content h5 {
+    margin: 0 !important;
+    padding: 0;
+    margin-right: 30px !important;
+}
+</style>
